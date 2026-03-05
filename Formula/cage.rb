9b9e869e04aa6499de2d9e3ab7619b88efef6811
Scott Schlesier <scott@schlesier.ca>
2026-03-04 21:06:24 -0800

    feat: bump cage to v0.2.2

--- Formula/cage.rb
+++ Formula/cage.rb
@@ -1,8 +1,8 @@
 class Cage < Formula
   desc "Run Claude Code inside an isolated Linux VM"
   homepage "https://github.com/sschlesier/cage"
-  url "https://github.com/sschlesier/cage/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "8f238be461d27f13c581aa2c288e65e315cbcd178fe31bfe2e7a44eadaa2b251"
+  url "https://github.com/sschlesier/cage/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "43bf19ab050aee1df4d3f6ed00b227b84fb12b21767cca068a36531f1bd27fe3"
   license :cannot_represent
 
   depends_on "container"
