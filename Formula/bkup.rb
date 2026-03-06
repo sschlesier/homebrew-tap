7e68b5d085b2a5140444fadb09728415359727d5
Scott Schlesier <scott@schlesier.ca>
2026-03-06 09:35:53 -0800

    feat: bump bkup to v1.1.0

--- Formula/bkup.rb
+++ Formula/bkup.rb
@@ -1,8 +1,8 @@
 class Bkup < Formula
   desc "Backup directories to a restic repository on Backblaze B2"
   homepage "https://github.com/sschlesier/bkup"
-  url "https://github.com/sschlesier/bkup/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "121bff0a16db6e90df6e42c6b1d00a9f8951fb75176bf7cdff3b0ea2fd971ac1"
+  url "https://github.com/sschlesier/bkup/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "36551148fb2ecee0993feb98acdc52950f4d099bfc7ccbab57f9571bedeccbff"
   license "MIT"
 
   depends_on "restic"
